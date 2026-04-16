6c3d8d0d2458f940ba10e13396dbb5861e7235ff
Rahul Raj <rahul_raj@newgensoft.com>
2026-04-16 06:40:43 +0530

    IC21

--- src/main/java/com/rahul/tourism/tourismapp/HomeController.java
+++ src/main/java/com/rahul/tourism/tourismapp/HomeController.java
@@ -8,6 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "🌍 Welcome to Rahul Spring Boot Tourism App || The whole New World!! || The Real New World🚀";
+      //  return "🌍 Welcome to Rahul Spring Boot Tourism App || The whole New World!! || The Real New World🚀";
+    return "=============Nothing=================="
     }
 }
